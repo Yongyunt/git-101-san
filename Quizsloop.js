bb3389d9ecce128a3ba71ea27d8cad6d13d4ae5a
Yongyoot <37680386+Yongyunt@users.noreply.github.com>
2020-09-16 15:03:45 +0700

    san
    loop quizs

---
+++ Quizsloop.js
@@ -0,0 +1,17 @@
+1
+for (let i = 1; i <21; i++ )
+console.log(i)
+
+// 2
+// for (let i = 1; i <21; i++ ){
+//         console.log(i % 2 === 0 ? i : "")
+// }
+
+// 3
+// let result1 = [-1,2,3,56,6,-93,0,4].map(el => el*2)
+// console.log(result1)
+
+// 4
+// let result2 = [10,13,44,55,89,0,12,1].filter(el => el/2)
+// console.log(result2)
+
