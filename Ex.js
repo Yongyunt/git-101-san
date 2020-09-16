51403549db09f7346d8f3ea72b2c722e13302763
Yongyoot <37680386+Yongyunt@users.noreply.github.com>
2020-09-16 13:42:12 +0700

    san

---
+++ Ex.js
@@ -0,0 +1,16 @@
+//console.log("Hello World")
+let name = "san"
+name = 'san'
+let test ="Hello"
+name = `${test}B`   
+console.log("name : ", name)
+
+let arr = ['apple','banana','kivi']
+console.log("arr : ", arr[1])
+
+let person = {
+    name: 'san',
+    age:15 ,
+    gender: 'male'
+}
+console.log(person.name)
